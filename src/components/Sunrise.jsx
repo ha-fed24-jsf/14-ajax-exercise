6c19633f7c21310a4091a7dac99b5508d5e4041c
David Andersson <lejonmanen@gmail.com>
2025-04-08 11:02:05 +0200

    Övning 6.1

--- src/components/Sunrise.jsx
+++ src/components/Sunrise.jsx
@@ -1,25 +1,54 @@
+import { useState } from "react"
+import { getCitySuntimes, getSchoolSuntimes } from "../data/api"
+
 const Sunrise = () => {
+	const [schoolTimes, setSchoolTimes] = useState(null)
+	const [cityTimes, setCityTimes] = useState(null)
+	const [city, setCity] = useState('Haparanda')
 
-
+	const handleGetSchool = () => {
+		getSchoolSuntimes(setSchoolTimes)
+	}
+	const handleGetCity = () => {
+		getCitySuntimes(setCityTimes, city)
+	}
+	
 	return (
 		<div className="sunrise">
 			<h2> Skolan </h2>
-			<p>
-				Idag gick solen upp klockan .... och förväntas gå ner klockan ...
-			</p>
-			<button> Hämta soltider för skolan </button>
+			<section>
+				{schoolTimes ? (
+					<p> Idag gick solen upp klockan {schoolTimes.sunrise} och förväntas gå ner klockan {schoolTimes.sunset} </p>
+				) : (
+					<p> Klicka på knappen för att hämta datan </p>
+				)}
+			</section>
+			<button onClick={handleGetSchool}> Hämta soltider för skolan </button>
 
 			<hr />
 
 			<h2> Geocoding </h2>
-			<p> Skriv namnet på en stad: <input type="text" value="Haparanda" /> <button> Men den här staden då? </button> </p>
-			<p> I Haparanda gick solen upp kl ... idag och förväntas gå ner kl .... </p>
-			<hr />
+			<p> Skriv namnet på en stad: 
+				<input type="text" 
+					value={city}
+					onChange={event => setCity(event.target.value)}
+					/>
+				<button onClick={handleGetCity}> Men den här staden då? </button>
+			</p>
+			<section>
+				{cityTimes ? (
+					<p> I "{cityTimes.city}" gick solen upp kl ... idag och förväntas gå ner kl .... </p>
+				) : (
+					<p> Vänligen skriv en stad och klicka! </p>
+				)}
+			</section>
+			
+			{/* <hr />
 
 			<h2> Reverse geocoding </h2>
 			<p> Skriv latitud och longitud: <input type="text" value="57.1" /> <input type="text" value="11.9" /> <button> Vad finns här? </button> </p>
 			<p> Verkar som att ... ligger på den platsen. </p>
-			<p> Solen gick upp kl ... idag och förväntas gå ner kl .... </p>
+			<p> Solen gick upp kl ... idag och förväntas gå ner kl .... </p> */}
 
 		</div>
 	)
--- src/data/api.js
+++ src/data/api.js
@@ -1,1 +1,59 @@
 // skolan ligger på latitud=57.6731597 och longitud=11.8787299
+// Standard URL:
+// https://api.sunrisesunset.io/json?lat=38.907192&lng=-77.036873
+
+const cageApiKey = '8145e82c69ce4aa1bb09c9199cc5d2ed'
+
+async function getSchoolSuntimes(saveData) {
+	const school = { lat: 57.6731597, lng: 11.8787299 }
+	const url = `https://api.sunrisesunset.io/json?lat=${school.lat}&lng=${school.lng}`
+	const settings = {}
+	try {
+		const response = await fetch(url, settings)
+			const data = await response.json()
+			// { results: { sunrise, sunset }}
+			const result = { sunrise: data.results.sunrise, sunset: data.results.sunset }
+			console.log('Datan från API: ', data)
+			console.log('Förenklad data: ', result)
+		saveData(result)
+
+	} catch(error) {
+		console.error('Fel från API:', error.message)
+	}
+}
+
+// async function getSuntimes(lat, lng)
+
+async function getCitySuntimes(saveData, address) {
+	const url = `https://api.opencagedata.com/geocode/v1/json?q=${address}&key=${cageApiKey}`
+	const settings = {}
+	try {
+		const response = await fetch(url, settings)
+		const data = await response.json()
+		console.log('Datan från API: ', data)
+		// { results: [ { formatted, geometry: { lat, lng } } ]}
+		const results = {
+			city: data.results[0].formatted,
+			lat: data.results[0].geometry.lat,
+			lng: data.results[0].geometry.lng,
+		}
+		console.log('Förenklad data: ', results)
+
+		const url2 = `https://api.sunrisesunset.io/json?lat=${results.lat}&lng=${results.lng}`
+		const response2 = await fetch(url2, settings)
+		const data2 = await response2.json()
+		// { results: { sunrise, sunset }}
+		
+		results.sunrise = data2.results.sunrise
+		results.sunset = data2.results.sunset
+		
+		saveData(results)
+		console.log('Förenklad data: ', results)
+
+	} catch(error) {
+		console.error('Fel från API:', error.message)
+	}
+
+}
+
+export { getSchoolSuntimes, getCitySuntimes }
